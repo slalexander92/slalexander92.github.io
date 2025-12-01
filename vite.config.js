716ab1ba54e27f8fb8c83c89607c526bab01d934
Spencer Alexander <slalexander92@gmail.com>
2025-11-30 22:53:14 -0800

    chore(.): update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/slalexander92.github.io/', // important for GitHub Pages
+  base: '/', // for username.github.io or a custom domain at root
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
