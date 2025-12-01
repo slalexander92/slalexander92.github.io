c6bc375329c14fedda53a8472ccd2ac048de0767
Spencer Alexander <slalexander92@gmail.com>
2025-11-30 23:02:03 -0800

    Deploy built site

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // for username.github.io or a custom domain at root
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
